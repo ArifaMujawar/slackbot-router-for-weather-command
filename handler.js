a868cbdb6826277c05d7416c75ed5d353f4d3f18
ArifaMujawar <arifabegam.mujawar@gmail.com>
2020-03-31 17:38:03 +0300

    handling error conditions

--- handler.js
+++ handler.js
@@ -1,44 +1,46 @@
-'use strict';
-const queryString = require('query-string');
-const forecast = require('./utils/forecast')
-const geocode = require('./utils/geocode')
+"use strict";
+const queryString = require("query-string");
+const forecast = require("./utils/forecast");
+const geocode = require("./utils/geocode");
 
 module.exports.hello = async event => {
   let response;
   let geoCodeResponse, forecastResponse;
   const result = queryString.parse(event.body);
-  if(result.text){
-    response = `You provided the following city: ${result.text}`
-   /**
-    * provide temperature for the particular cit
-    * error cases: to be handled
-    *   invalid city name,
-    *   service not available
-    *   weather app of nodejs
-    *  */ 
-    geoCodeResponse = await geocode(result.text);
-    console.log(`response from geo-code is ${geoCodeResponse}`);
-    forecastResponse = await forecast(geoCodeResponse.latitude, geoCodeResponse.longitude, result.text)
-    
+  const cityName = result.text;
+  if (cityName) {
+    try {
+      geoCodeResponse = await geocode(cityName);
+      console.log("response from geo-code is", geoCodeResponse);
+      response = await forecast(
+        geoCodeResponse.latitude,
+        geoCodeResponse.longitude,
+        cityName
+      );
+    } catch (error) {
+      console.log(`failed to fetch weather data of ${cityName}`, error);
+      response = error;
+    }
+  } else {
+    response = "Please provide a city name.";
+    console.log("Please provide city");
   }
-  else{
-    response = 'Please provide a city name.'
-  }
- 
+
   const responseToSlack = {
-    blocks: [ 
+    blocks: [
       {
         type: "section",
         text: {
           type: "mrkdwn",
           text: response
         }
-    }]
-  }
- 
+      }
+    ]
+  };
+
   return {
     statusCode: 200,
-    body: JSON.stringify(forecastResponse),
+    body: JSON.stringify(responseToSlack)
   };
 };
 //  https://lidd86rav0.execute-api.us-east-1.amazonaws.com/dev/helloworld
--- utils/forecast.js
+++ utils/forecast.js
@@ -5,8 +5,10 @@ const forecast = (latitude, longitude, InputAddress) => {
     const url = `https://api.darksky.net/forecast/${process.env.darkSkyApiKey}/${latitude},${longitude}?units=si`;
     request({ url: url, json: true }, (error, { body }) => {
       if (error) {
+        console.log('Unable to connect to weather services');
         reject("Unable to connect to weather services");
       } else if (body.error) {
+        console.log("Unable to find location")
         reject("Unable to find location");
       } else {
         const result = `In ${InputAddress}, it is currently ${body.currently.temperature}℃. The high of today is ${body.daily.data[0].temperatureHigh}℃ with a low of ${body.daily.data[0].temperatureLow}℃. There is a ${body.currently.cloudCover}% chance of rain.`;
