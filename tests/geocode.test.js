8bb4fcc4a2613d5dd21e422ccf74b7972b81bc30
ArifaMujawar <arifabegam.mujawar@gmail.com>
2020-04-07 22:37:06 +0300

    geocode test

---
+++ tests/geocode.test.js
@@ -0,0 +1,14 @@
+const request = require("request");
+const forecast = require('../utils/geocode')
+
+test('should return right temperature ', ()=>{
+  const data = forecast(12, 14,'espoo');
+  expect(data).toEqual('In espoo, it is currently 7.1℃. The high of today is 11.57℃ with a low of 3.15℃. There is a 0.93% chance of rain.')
+});
+test('should return unable to find location', ()=>{
+
+});
+
+test('should return unable to connect weather services', ()=>{
+
+});
