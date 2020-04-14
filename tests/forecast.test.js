2c1c83f04bc221930c05c62e8a41272086089a23
ArifaMujawar <arifabegam.mujawar@gmail.com>
2020-04-14 19:11:55 +0300

    forecast,geocode test written

--- tests/forecast.test.js
+++ tests/forecast.test.js
@@ -1,15 +1,58 @@
 const request = require("request");
-const forecast = require('../utils/forecast')
+const forecast = require("../utils/forecast");
 
-test('should return right temperature ', ()=>{
-  const data = forecast(12, 14,'espoo');
-  expect(data).toEqual('In espoo, it is currently 7.1℃. The high of today is 11.57℃ with a low of 3.15℃. There is a 0.93% chance of rain.')
+jest.mock("request");
+
+test("should return right temperature ", async () => {
+  request.mockImplementation((params, callback) => {
+    callback(null, {
+      body: {
+        currently: {
+          temperature: 7.1,
+          cloudCover: 0.93,
+        },
+        daily: {
+          data: [
+            {
+              temperatureHigh: 11.57,
+              temperatureLow: 3.15,
+            },
+          ],
+        },
+      },
+    });
+  });
+  let data = await forecast(12, 14, "espoo");
+  expect(data).toEqual(
+    "In espoo, it is currently 7.1℃. The high of today is 11.57℃ with a low of 3.15℃. There is a 0.93% chance of rain."
+  );
 });
 
-test('should return unable to find location', ()=>{
+test("should return unable to find location", async () => {
+  request.mockImplementation((params, callback) => {
+    callback(null, {
+      body: {
+        error: "Unable to find location",
+      },
+    });
+  });
 
+  await expect(forecast(-0, 0, "north pole")).rejects.toBe(
+    "Unable to find location"
+  );
+  
 });
 
-test('should return unable to connect weather services', ()=>{
+test("should return unable to connect weather services", async() => {
+  request.mockImplementation((params, callback) => {
+   callback('error', {body: { error: true}} )
+  })
 
+  try {
+    await forecast(0, 0, 'north pole');
+    expect(true).toBe(false);
+  } catch (error) {
+    expect(error).toBe('Unable to connect to weather services')
+  }
+  //await expect(forecast(-0, 0, "north pole").rejects.toBe('Unable to connect to weather services'))
 });
--- tests/geocode.test.js
+++ tests/geocode.test.js
@@ -1,14 +1,49 @@
 const request = require("request");
-const forecast = require('../utils/geocode')
+const geocode = require("../utils/geocode");
 
-test('should return right temperature ', ()=>{
-  const data = forecast(12, 14,'espoo');
-  expect(data).toEqual('In espoo, it is currently 7.1℃. The high of today is 11.57℃ with a low of 3.15℃. There is a 0.93% chance of rain.')
+jest.mock("request");
+
+test("should return right location, longitude, latitude ", async () => {
+  request.mockImplementation((params, callback) => {
+    callback(null, {
+      body: {
+        features: [
+          {
+            center: [123, 12],
+            place_name: "belgavi",
+          },
+        ],
+      },
+    });
+  });
+  let data = await geocode('belgavi')
+  expect(data).toEqual({
+    latitude: 12,
+    longitude: 123,
+    location: "belgavi",
+  });
 });
-test('should return unable to find location', ()=>{
-
+test("should return unable to find location", async () => {
+  request.mockImplementation((params, callback) => {
+    callback(null, {
+      body: {
+        features: [],
+      },
+    });
+  });
+  await expect(geocode("llll")).rejects.toBe(
+    "Unable to find location. Try another search"
+  );
 });
 
-test('should return unable to connect weather services', ()=>{
-
+test("should return unable to connect location services", async () => {
+  request.mockImplementation((params, callback) => {
+    callback("error", { body: { error: true } });
+  });
+  try {
+    await geocode("aaa");
+    expect(true).toBe(false);
+  } catch (error) {
+    expect(error).toBe("Unable to connect to location services");
+  }
 });
